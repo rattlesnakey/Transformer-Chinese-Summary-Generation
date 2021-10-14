1dc04c6f0d3ab1c9940c13fd5d91abff0cc17a38
rattlesnakey <hengyuan_blcu@yeah.net>
2021-10-14 21:02:47 +0800

    update prepare_for_fairseq.sh

--- scripts/prepare_for_fairseq.sh
+++ scripts/prepare_for_fairseq.sh
@@ -4,11 +4,14 @@ set -e
 ORIGIN_DATA_PREFIX=../crime_data/origin
 PREPARE_DATA_PREFIX=../crime_data/for_fairseq/prepare
 BPE_DIR=../crime_data/for_fairseq/bpe
-BPE_MODEL=../../fairseq-bart/pretrained-models # 改
+BPE_MODEL=../models/sentencepiece/law.sentencepiece.model
 BIN_DIR=../crime_data/for_fairseq/data-bin
-DICT=pretrained-models/mbart.cc25.v2/dict.txt # 改
+# DICT=../models/sentencepiece/law.sentencepiece.vocab
 for file in train dev test; do
-    python -u ../src/process.py --input ${ORIGIN_DATA_PREFIX}/${file}.json --output ${PREPARE_DATA_PREFIX}/${file}
+    python -u ../src/process.py \
+    --input ${ORIGIN_DATA_PREFIX}/${file}.json \
+    --output ${PREPARE_DATA_PREFIX}/${file} \
+    --task fairseq
     done
 
 if [ -d ${BPE_DIR} ]; then
@@ -21,15 +24,16 @@ for file in `ls ${PREPARE_DATA_PREFIX}`; do
     spm_encode --model=${BPE_MODEL} \
         < ${PREPARE_DATA_PREFIX}/${file} \
         > ${BPE_DIR}/${file}
+    done
 
-
+if [ -d ${BIN_DIR} ]; then 
+    rm -rf ${BIN_DIR}
+fi
 fairseq-preprocess \
     --source-lang src \
     --target-lang tgt \
     --trainpref ${BPE_DIR}/train \
-    --validpref ${BPE_DIR}/valid \
+    --validpref ${BPE_DIR}/dev \
     --testpref ${BPE_DIR}/test \
     --destdir ${BIN_DIR} \
-    --srcdict ${DICT} \
-    --tgtdict ${DICT} \
     --workers 20
