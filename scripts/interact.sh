c7525f6549ea7a5df0ee3203d23db46505d05e55
rattlesnakey <hengyuan_blcu@yeah.net>
2021-11-16 20:46:02 +0800

    modify interact.sh

--- scripts/interact.sh
+++ scripts/interact.sh
@@ -4,8 +4,8 @@ set -e
 MODEL_DIR=../models
 OUTPUT_PATH=../results/interactive
 DATA_DIR=../crime_data/for_fairseq/data-bin
-CUDA_VISIBLE_DEVICES=4 fairseq-interactive \
-    --path ${MODEL_DIR}/checkpoints/checkpoint68.pt ${DATA_DIR}\
+CUDA_VISIBLE_DEVICES=4 fairseq-interactive ${DATA_DIR} \
+    --path ${MODEL_DIR}/checkpoints/checkpoint68.pt \
     --beam 5 --remove-bpe \
     --results-path ${OUTPUT_PATH} \
     --skip-invalid-size-inputs-valid-test
